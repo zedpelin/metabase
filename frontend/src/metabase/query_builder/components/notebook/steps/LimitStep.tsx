87758b5f3f9d08e65bc8acf3a9307cdb4f52fc58
Anton Kulyk <kuliks.anton@gmail.com>
2023-03-28 17:17:27 +0100

    Use metabase-lib v2 in `LimitStep`

--- frontend/src/metabase/query_builder/components/notebook/steps/LimitStep.tsx
+++ frontend/src/metabase/query_builder/components/notebook/steps/LimitStep.tsx
@@ -2,24 +2,29 @@ import React from "react";
 import { t } from "ttag";
 
 import LimitInput from "metabase/query_builder/components/LimitInput";
-import type StructuredQuery from "metabase-lib/queries/StructuredQuery";
+import type { NotebookStep } from "metabase/query_builder/components/notebook/lib/steps.types";
+
+import * as Lib from "metabase-lib";
+import type { Query } from "metabase-lib/types";
 
 import { NotebookCell } from "../NotebookCell";
 
 interface LimitStepProps {
-  query: StructuredQuery;
+  step: NotebookStep;
   color: string;
-  updateQuery: (query: StructuredQuery) => void;
+  updateQuery: (query: Query) => void;
 }
 
-function LimitStep({ color, query, updateQuery }: LimitStepProps) {
-  const limit = query.limit();
+function LimitStep({ step, color, updateQuery }: LimitStepProps) {
+  const { topLevelQuery: query, stageIndex } = step;
+
+  const limit = Lib.currentLimit(query, step.stageIndex);
   const value = typeof limit === "number" ? limit : "";
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const limit = parseInt(e.target.value, 10);
     if (limit >= 1) {
-      updateQuery(query.updateLimit(limit));
+      updateQuery(Lib.limit(query, stageIndex, limit));
     }
   };
 
