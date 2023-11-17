70f7fb6bd858f8c38f7c48cb0b6faffa1e8eda16
Aleksandr Lesnenko <alxnddr@gmail.com>
2023-11-17 13:29:07 -0300

    Support histogram X-axis, improve X-axis type inferrence

--- frontend/src/metabase/visualizations/echarts/cartesian/option/axis.ts
+++ frontend/src/metabase/visualizations/echarts/cartesian/option/axis.ts
@@ -213,7 +213,6 @@ const getXAxisType = (settings: ComputedVisualizationSettings) => {
     case "linear":
       return "value";
     default:
-      // TODO: implement histogram
       return "category";
   }
 };
@@ -240,6 +239,7 @@ export const buildDimensionAxis = (
 
   const boundaryGap =
     axisType === "value" ? undefined : ([0.02, 0.02] as [number, number]);
+  // const boundaryGap = false;
 
   const nameGap = getXAxisNameGap(
     chartModel,
