49486cf8d42bbb2afa86962c5128a13b40293c2e
John Swanson <john.swanson@metabase.com>
2023-11-20 12:12:34 -0800

    WIP: fix tests for loading with extra keys
    This is not the ideal way to fix things, but I'm committing for now as a milepost.
    
    There were two problems with the existing code:
    
    - running the test left the database in a state that caused subsequent tests to fail :sob:
    
    - running the test _without_ first running `dev/start!` would fail
    
    This led to ... extreme confusion, as I'd try to rerun other failing tests and they'd pass, then try running these
    tests and they'd fail... or sometimes pass, depending on if I'd run `start!` from my REPL or not... and then try
    running the original failing tests again and they'd fail. Just a lot of degrees of freedom, leading to confusion.
    
    The code here is not ideal, obviously - we're digging into the internals of the startup process to put the database
    into the state we expect. I'll look into this more tomorrow.

--- enterprise/backend/test/metabase_enterprise/serialization/cmd_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/cmd_test.clj
@@ -2,10 +2,13 @@
   (:require
    [clojure.java.io :as io]
    [clojure.test :refer :all]
+   [metabase-enterprise.audit-db :as adb]
    [metabase-enterprise.serialization.cmd :as serialization.cmd]
    [metabase-enterprise.serialization.load :as load]
    [metabase-enterprise.serialization.test-util :as ts]
    [metabase.cmd :as cmd]
+   [metabase.db :as mdb]
+   [metabase.db.schema-migrations-test.impl :as schema-migrations-test.impl]
    [metabase.models :refer [Card Dashboard DashboardCard Database User]]
    [metabase.public-settings.premium-features-test :as premium-features-test]
    [metabase.test :as mt]
@@ -70,13 +73,21 @@
          (spit ~path original#)))))
 
 (deftest loading-does-not-fail-on-extra-yaml-keys
-  (testing "an extra key does not break loading"
-    (premium-features-test/with-premium-features #{:serialization}
-      (is (serialization.cmd/v2-load-internal! "resources/instance_analytics/" {} :token-check? false))
-
+  (premium-features-test/with-premium-features #{:serialization}
+    (testing "it works without an extra key (sanity check)"
+      (schema-migrations-test.impl/with-temp-empty-app-db [_conn :h2]
+        ;; NOTE: this is probably not the ideal way to set things up. It seems that we need to set up the DB and
+        ;; install the audit DB before we run `v2-load-internal`, but this is clearly awkard.
+        (mdb/setup-db!)
+        (#'adb/maybe-install-audit-db)
+        (is (serialization.cmd/v2-load-internal! "resources/instance_analytics/" {} :token-check? false))))
+    (testing "and it works with an extra key"
       (let [path "resources/instance_analytics/collections/vG58R8k-QddHWA7_47umn_metabase_analytics/vG58R8k-QddHWA7_47umn_metabase_analytics.yaml"]
         (with-file-updated [path #(-> % yaml/parse-string (assoc :foo? "bar") yaml/generate-string)]
-          (is (serialization.cmd/v2-load-internal! "resources/instance_analytics/" {} :token-check? false)))))))
+          (schema-migrations-test.impl/with-temp-empty-app-db [_conn :h2]
+            (mdb/setup-db!)
+            (#'adb/maybe-install-audit-db)
+            (is (serialization.cmd/v2-load-internal! "resources/instance_analytics/" {} :token-check? false))))))))
 
 (deftest blank-target-db-test
   (testing "Loading a dump into an empty app DB still works (#16639)"
